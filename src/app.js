19051f8c3e92d9e85b66cfc5ae99b8e2bcec1038
Hector <127518350+hector20gonza@users.noreply.github.com>
2024-03-22 08:24:17 -0400

    second

--- src/app.js
+++ src/app.js
@@ -11,6 +11,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(morgan("dev"));
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use("/", router);
 
 module.exports = {
